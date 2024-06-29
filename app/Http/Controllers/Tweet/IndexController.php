955ab3a9bab78016e988196fdf46398db3c99352
Ein Kaung <einkaung.mce@gmail>
2024-06-29 15:38:13 +0900

    app/Http/Controllers/Tweet/IndexController.php modified

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -14,8 +14,6 @@ class IndexController extends Controller
     public function __invoke(Request $request)
     {
         $tweets = Tweet::orderBy('created_at', 'DESC')->get();
-        // dd($tweets);
-        //
         return view('tweet.index')
             ->with('tweets', $tweets);
     }
