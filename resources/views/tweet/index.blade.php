2fdb2fbc6f2ca8db1026a3a3736fd9fe682c095d
Ein Kaung <einkaung.mce@gmail>
2024-07-27 11:19:41 +0900

    index.blade -> @if (session) @endif 直した

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -10,6 +10,9 @@
 @auth
     <div>
         <p>投稿フォーム</p>
+        @if (session('feedback.success'))
+            <p style="color: green">{{ session('feedback.success') }}</p>
+        @endif
         <form action="{{ route('tweet.create') }}" method="post">
             @csrf
             <label for="tweet-content">つぶやき</label>
