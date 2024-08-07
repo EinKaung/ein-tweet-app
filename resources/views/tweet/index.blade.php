0fb7c9b7672e99fa8a71355e5db066322658ca68
Ein Kaung <einkaung.mce@gmail>
2024-08-07 20:14:03 +0900

    pg149-> css and js links COMMENTED OUT in tweet/index.blade.php

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>つぶやきアプリ</title>
-    <link href="/css/app.css" rel="stylesheet">
-    <script src="/js/app.js"></script>
+    <!-- <link href="./css/app.css" rel="stylesheet">
+    <script src="./js/app.js"></script> -->
 </head>
 <body>
 <h1>つぶやきアプリ</h1>
