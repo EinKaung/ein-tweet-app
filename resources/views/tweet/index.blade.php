73db92671ab638c474cf73dec629d606d8e61e3a
Ein Kaung <einkaung.mce@gmail>
2024-10-05 14:16:39 +0900

    [add] app.css and app.js in resources and @vite in tweet/index

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>つぶやきアプリ</title>
-    <!-- <link href="./css/app.css" rel="stylesheet">
-    <script src="./js/app.js"></script> -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
 <h1>つぶやきアプリ</h1>
