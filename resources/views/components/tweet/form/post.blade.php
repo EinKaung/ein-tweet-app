34053cc4a2df26975fe1fc5c2741011ec127f8d6
Ein Kaung <einkaung.mce@gmail>
2024-10-05 16:25:51 +0900

    [add]#textbook_157pg_button-a.blade

--- resources/views/components/tweet/form/post.blade.php
+++ resources/views/components/tweet/form/post.blade.php
@@ -3,7 +3,7 @@
         <form action="{{ route('tweet.create') }}" method="post">
             @csrf
             <div class="mt-1">
-                <textarea 
+                <textarea
                     name="tweet"
                     rows="3"
                     class="focus:ring-teal-400 focus:border-teal-400 mt-1 block
@@ -17,7 +17,7 @@
             @error('tweet')
             <x-alert.error>{{ $message }}</x-alert.error>
             @enderror
-            
+
             <div class="flex flex-wrap justify-end">
                 <x-element.button>
                     つぶやき
@@ -26,3 +26,11 @@
         </form>
     </div>
 @endauth
+{{-- @guest
+<div class="flex flex-wrap justify-center">
+    <div class="w-1/2 p-4 flex flex-wrap justify-envenly">
+        <x-element.button-a :href="route('login')">ログイン</x-element.button-a>
+        <x-element.button-a :href="route('register')" theme="secondary">会員登録</x-element.button-a>
+    </div>
+</div>
+@endguest --}}
