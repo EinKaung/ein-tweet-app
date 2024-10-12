2fe18620c0fd7d274f99a3f7b665dcabe2413205
Ein Kaung <einkaung.mce@gmail>
2024-10-12 13:31:58 +0900

    [fix]#pg158_ログインと会員登録ボタン表示できた

--- resources/views/components/element/button-a.blade.php
+++ resources/views/components/element/button-a.blade.php
@@ -1,10 +1,10 @@
 @props([
-    'gref' => '',
+    'href' => '',
     'theme' => 'primary',
 ])
 
 @php
-    if (!function_exists('getThemeClassForButtonA')) {
+    if(!function_exists('getThemeClassForButtonA')) {
         function getThemeClassForButtonA($theme) {
             return match ($theme) {
                 'primary' => 'text-white bg-blue-500 hover:bg-blue-600
